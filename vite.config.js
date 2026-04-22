18aa3772c28efcc164ba068adca1f83cc358e7af
darren-churchy <78827224+darren-churchy@users.noreply.github.com>
2026-04-22 10:44:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // This is required for GitHub Pages to resolve assets correctly
 export default defineConfig({
   plugins: [react()],
-  base: '/darren-churchy/policy-evaluation-feasibility-tool/',
+  base: '/policy-evaluation-feasibility-tool/',
 })
