f460659a6f890e5ee152ee8fa1332a772d9abf29
darren-churchy <78827224+darren-churchy@users.noreply.github.com>
2026-04-23 10:29:16 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace 'evaluation-feasibility-tool' with your actual GitHub repository name
-// This is required for GitHub Pages to resolve assets correctly
-export default defineConfig({
-  plugins: [react()],
-  base: '/policy-evaluation-feasibility-tool/',
-})
